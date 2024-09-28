0dbb2f43bff2d8afad1f1b6eb7923371687dba27
wev2530 <wev049665@gmail.com>
2024-09-28 13:11:33 +0100

    modified:   index.css 	modified:   index.html 	modified:   index.js

--- index.js
+++ index.js
@@ -23,3 +23,17 @@ const gmitHome = document.querySelector(".gmit-home");
 gmitHome.addEventListener("click", function(){
     window.location.href = "https://greatmindsintechfoundation.webflow.io/"
 })
+
+
+const bookASeat = document.querySelector(".group-e8");
+
+bookASeat.addEventListener("click", function(){
+    window.location.href = "https://lu.ma/49zk9vh8/"
+})
+
+
+const bookASeat2 = document.querySelector(".rectangle-e2");
+
+bookASeat2.addEventListener("click", function(){
+    window.location.href = "https://lu.ma/49zk9vh8/"
+})
