493aaac922fbaa895b3b6b820d3fb6c88ea7a8fb
wev2530 <wev049665@gmail.com>
2024-09-28 12:23:08 +0100

    modified:   index.js

--- index.js
+++ index.js
@@ -9,3 +9,17 @@ const spPage = document.querySelector(".speakers");
 spPage.addEventListener("click", function(){
     window.location.href = "https://gmit-conf-speakers.netlify.app/"
 })
+
+
+const aboutPage = document.querySelector(".about");
+
+aboutPage.addEventListener("click", function(){
+    window.location.href = "https://gmit-conf-speakers.netlify.app/"
+});
+
+
+const gmitHome = document.querySelector(".gmit-home");
+
+spPage.addEventListener("click", function(){
+    window.location.href = "https://greatmindsintechfoundation.webflow.io/"
+})
