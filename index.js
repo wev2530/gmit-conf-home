38f578185247551c77c57a0ee3e39987de71c34b
wev2530 <wev049665@gmail.com>
2024-09-28 12:15:30 +0100

    modified:   index.js

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ const regPage = document.querySelector(".register-competition");
 
 regPage.addEventListener("click", function(){
     window.location.href = "https://gmit-conf-registration.netlify.app/";
+});
+
+const spPage = document.querySelector(".speakers");
+
+spPage.addEventListener("click", function(){
+    window.location.href = "https://gmit-conf-speakers.netlify.app/"
 })
